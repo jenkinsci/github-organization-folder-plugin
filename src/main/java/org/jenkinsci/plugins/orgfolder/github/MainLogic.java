18373825a032255c2568ce264b5f53a7427923f2
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-06-28 13:36:43 -0700

    add/remove should be matching up
    This change makes sure that there's no code path in which the execution
    escapes functions without calling corresponding removal.

--- src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/MainLogic.java
@@ -119,11 +119,11 @@ public class MainLogic {
      */
     public void applyRepo(WorkflowMultiBranchProject item, GitHubSCMNavigator scm) throws IOException {
         if (UPDATING.get().add(item)) {
-            GitHub hub = connect(item, scm);
-            GHRepository repo = hub.getRepository(scm.getRepoOwner() + '/' + item.getName());
-
             BulkChange bc = new BulkChange(item);
             try {
+                GitHub hub = connect(item, scm);
+                GHRepository repo = hub.getRepository(scm.getRepoOwner() + '/' + item.getName());
+
                 item.setIcon(new GitHubRepoIcon());
                 item.replaceAction(new GitHubRepoAction(repo));
                 item.replaceAction(new GitHubLink("repo",repo.getHtmlUrl()));
@@ -162,11 +162,11 @@ public class MainLogic {
      */
     public void applyBranch(WorkflowJob branch, WorkflowMultiBranchProject repo, GitHubSCMNavigator scm) throws IOException {
         if (UPDATING.get().add(branch)) {
-            Branch b = repo.getProjectFactory().getBranch(branch);
-            GitHubLink repoLink = repo.getAction(GitHubLink.class);
-            if (repoLink!=null) {
-                BulkChange bc = new BulkChange(branch);
-                try {
+            BulkChange bc = new BulkChange(branch);
+            try {
+                Branch b = repo.getProjectFactory().getBranch(branch);
+                GitHubLink repoLink = repo.getAction(GitHubLink.class);
+                if (repoLink!=null) {
                     ChangeRequestAction action = b.getHead().getAction(ChangeRequestAction.class);
                     String url;
                     if (action == null) {
@@ -176,10 +176,10 @@ public class MainLogic {
                     }
                     branch.replaceAction(new GitHubLink("branch", url));
                     bc.commit();
-                } finally {
-                    bc.abort();
-                    UPDATING.get().remove(branch);
                 }
+            } finally {
+                bc.abort();
+                UPDATING.get().remove(branch);
             }
         }
     }
