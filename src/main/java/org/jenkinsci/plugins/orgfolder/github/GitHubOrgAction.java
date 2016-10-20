18eae16dc595773caae24254c952eebdda85836d
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-10-20 12:28:40 +0100

    findbugs, quit your whining

--- src/main/java/org/jenkinsci/plugins/orgfolder/github/GitHubOrgAction.java
+++ src/main/java/org/jenkinsci/plugins/orgfolder/github/GitHubOrgAction.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.plugins.orgfolder.github;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.model.InvisibleAction;
 import java.io.ObjectStreamException;
 import jenkins.branch.OrganizationFolder;
@@ -16,6 +17,7 @@ import java.net.URL;
  * @deprecated use {@link org.jenkinsci.plugins.github_branch_source.GitHubOrgAction}
  */
 @Deprecated
+@SuppressFBWarnings("NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
 public class GitHubOrgAction extends org.jenkinsci.plugins.github_branch_source.GitHubOrgAction {
 
     GitHubOrgAction(GHUser org) throws IOException {
